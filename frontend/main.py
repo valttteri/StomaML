ca5a39d7052345abd209eec8b7283118898350ae
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2026-03-05 17:10:53 +0200

    New file for utils and button for clearing files

--- frontend/main.py
+++ frontend/main.py
@@ -8,79 +8,7 @@ import requests
 import streamlit as st
 import streamlit_ext as ste
 from config import API_URL
-
-class ProgressBar:
-    """
-    Class for inference progress bar
-
-    Params:
-    value: 0 <= x <= 100
-    text: a string displayed on top of the progress bar
-    width: "stretch" or int
-    """
-    def __init__(self, value: int, text: str, width: str):
-        self.value = value
-        self.text = ""
-        self.width = width
-        self.progress_bar = st.progress(value=self.value, text=self.text, width=self.width)
-    
-    def render(self):
-        return self.progress_bar
-
-    def update(self, value: int, text=None):
-        self.value = value
-
-        if text is not None:
-            #self.text = text
-            self.text = ""
-
-        self.progress_bar = self.progress_bar.progress(value=self.value, text=self.text, width=self.width)
-        return self.progress_bar
-    
-class RemainingTime:
-    """
-    Class for approximating the remaining inference time
-
-    Params:
-    total_files: the number of uploaded files
-    """
-    def __init__(self, total_files: int):
-        self.total_files = total_files
-        self.processed_files = 0
-        self.times = deque()
-
-    def get_update(self, time: datetime):
-        """
-        Update the remaining time approximation
-
-        Params:
-        time: The inference time of a single image
-        """
-        self.processed_files += 1
-        self.times.append(time)
-        
-        if len(self.times) < 5: # Don't return an approximation immediately
-            return "..."
-        if len(self.times) > 15: # Only keep track of 15 latest inference times
-            self.times.popleft()
-
-        total_time = sum(self.times, timedelta())
-        
-        average_time = total_time / len(self.times)
-        remaining_files = self.total_files - self.processed_files
-        
-        # An approximation of how much longer the inference will take
-        remaining_time = remaining_files * average_time
-
-        # Format remaining time as X min Y s
-        total_seconds = remaining_time.total_seconds()
-        minutes = int(total_seconds // 60)
-        seconds = int(total_seconds % 60)
-
-        formatted_time = f"{minutes} min {seconds} s"
-
-        return formatted_time
-
+from utils import RemainingTime, ProgressBar
 
 def call_api_with_files(
         file_paths: list[Path],
@@ -120,11 +48,13 @@ def download_csv(df: pd.DataFrame):
     """
     csv_bytes = df.to_csv(index=True).encode("utf-8")
 
-    return ste.download_button(
-        "Download CSV",
+    return st.download_button(
+        label="CSV",
         data=csv_bytes,
         file_name="stoma_results.csv",
         mime="text/csv",
+        on_click="ignore",
+        icon=":material/download:"
     )
 
 def download_metadata(results: list):
@@ -136,11 +66,13 @@ def download_metadata(results: list):
     """
     json_bytes = json.dumps(results, indent=2).encode("utf-8")
 
-    return ste.download_button(
-        label="Download Metadata",
+    return st.download_button(
+        label="Metadata",
         data=json_bytes,
         file_name="metadata.json",
-        mime="application/json"
+        mime="application/json",
+        on_click="ignore",
+        icon=":material/download:"
     )
 
 def main():
@@ -149,10 +81,14 @@ def main():
     if "inference_done" not in st.session_state:
         st.session_state["inference_done"] = False
 
+    if "file_uploader" not in st.session_state: # State variable used for resetting the file uploader
+        st.session_state["file_uploader"] = 1
+
     uploaded_files = st.file_uploader(
         "Upload files for analysis",
         accept_multiple_files=True,
         type=["jpg", "jpeg", "png"],
+        key=st.session_state["file_uploader"]
     )
 
     # metadata_arr: The inference metadata
@@ -160,24 +96,44 @@ def main():
     # density_info_arr: data to be included in the csv file
     metadata_arr, filename_arr, density_info_arr = [], [], []
 
-    # Slider for defining image scale in um / pixel
-    scale_slider = st.slider(
-        label="Image Scale",
-        min_value=5,
-        max_value=30,
-        value=10,
-        step=5,
-        format="%d $\mu$m",
-        width=200
-    )
-    scale_value = scale_slider / 46
+    # Columns for Image scale, Files uploaded and Clear files
+    col1, col2 = st.columns(spec=[1, 1], gap="medium", width=600) 
 
-    if st.button("Run analysis") and uploaded_files:
+    # Slider for defining image scale in um / pixel
+    with col1:
+        scale_value = st.number_input(
+            label="Image scale ($\mu$*m* / pixel)",
+            min_value=0.0000000001,
+            max_value=10.0,
+            value=10/46,
+            step=0.0000000001,
+            format="%0.10f",
+            placeholder="Enter value"
+        )
+
+    with col2:
+        # Subcolumns for better positioning of components
+        sub_col1, sub_col2 = st.columns([1, 2])
+        with sub_col1:
+            st.metric(label="Files uploaded", value=len(uploaded_files)) # Display the number of uploaded files
+        with sub_col2:
+            if len(uploaded_files) > 0 and st.button(label="Reset", icon=":material/delete:"):
+                st.session_state["file_uploader"] += 1
+                st.rerun()
+    
+
+    if st.button("Run analysis", icon=":material/bolt:") and uploaded_files:
         st.session_state["inference_done"] = False
-        # Initialize a progress bar
+
+        # Initialize an inference time approximator
         time_approximator = RemainingTime(total_files=len(uploaded_files))
+
+        # Message displayed on top of the progress bar
         inference_status = st.empty()
-        progress_bar = ProgressBar(0, "Inference in progress. Time remaining:", "stretch")
+        inference_status.markdown("Analysis in progress<br>Time remaining: ...", unsafe_allow_html=True)
+
+        # Initialize a progress bar
+        progress_bar = ProgressBar(0, "stretch")
         progress_bar.render()
 
 
@@ -186,7 +142,7 @@ def main():
 
             for idx, uf in enumerate(uploaded_files):
                 # Ratio is used for rendering the progress bar
-                ratio = int(round(idx/len(uploaded_files), 2)*100)
+                ratio = int(round((idx+1)/len(uploaded_files), 2)*100)
 
                 suffix = Path(uf.name).suffix or ".png"
                 with tempfile.NamedTemporaryFile(dir=tmpdir, suffix=suffix, delete=False) as tf:
@@ -199,9 +155,9 @@ def main():
                     start = datetime.now()
                     api_json = call_api_with_files(temp_path, conf=0.25, scale=scale_value)
                     end = datetime.now()
-                    
-                    total_time = end - start
-                    time_left = time_approximator.get_update(time=total_time) # Get an approximation of remaining inference time
+
+                    inference_time = end - start
+                    time_left = time_approximator.get_update(time=inference_time) # Get an approximation of remaining inference time
 
                     response_data = api_json["results"][0]
                     filename = uf.name
@@ -215,16 +171,17 @@ def main():
 
                     # Update the progress bar and inference status
                     progress_bar.update(value=ratio)
-                    inference_status.markdown(f"Inference in progress<br>Time remaining: {time_left}", unsafe_allow_html=True)
+                    inference_status.markdown(f"Analysis in progress<br>Time remaining: {time_left}", unsafe_allow_html=True)
 
             progress_bar.update(value=100)
-            inference_status.write("Inference done!")
+            inference_status.markdown("Analysis done!<br>Time remaining: 0 min 0 s", unsafe_allow_html=True)
             st.session_state["inference_done"] = True
+    
 
     # Display inference results once they are ready
     if st.session_state["inference_done"]:
         #st.json(results)
-        st.subheader("Results table")
+        st.subheader("Analysis results")
 
         df_cols = [
             "total_pixels",
@@ -242,7 +199,7 @@ def main():
 
         st.dataframe(df, use_container_width=True)
 
-        col1, col2 = st.columns(spec=[1, 1], width=250)
+        col1, col2 = st.columns(spec=[0.35, 0.65], gap="small", width=250) # Columns for download buttons
 
         # Button for downloading csv
         with col1:
